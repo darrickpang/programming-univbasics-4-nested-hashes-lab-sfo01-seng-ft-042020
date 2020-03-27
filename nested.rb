8353b6bd754517604785d7b32b2bb77746217ada
Darrick Pang <dkypang@yahoo.com>
2020-03-27 06:17:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
       }
     }
 end
-hopper[:grace_hopper]
+hopper["grace_hopper"]["languages"]
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
